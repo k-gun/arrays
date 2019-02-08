db70c7808e1937747b5b040ebc4b91ea82baaa86
Kerem Güneş <k-gun@mail.com>
2019-02-08 16:20:55 +0300

    Fix uses.

--- src/TypedArray.php
+++ src/TypedArray.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace arrays;
 
-use arrays\{AbstractArray, Type, TypeException};
+use arrays\{
+    Type, AbstractArray };
+use arrays\exception\{ TypeException };
 
 /**
  * @package arrays
