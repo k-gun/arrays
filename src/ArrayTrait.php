5bb8de60b8bed0b4f8ac6227f95622429fdf1e86
Kerem Güneş <k-gun@mail.com>
2019-02-08 17:35:07 +0300

    Disable strict mode, add new methods.

--- src/ArrayTrait.php
+++ src/ArrayTrait.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types=0);
 
 namespace arrays;
 
@@ -13,38 +13,42 @@ trait ArrayTrait
     // @return int|string|null
     protected final function _search($value)
     {
-        foreach ($this as $key => $_value) {
-            // make a strict search
-            if ($value === $_value) { return $key; }
+        return (false !== $key = array_search($value, $this->toArray(), true)) ? $key : null;
+    }
+
+    private final function _index($value, $_values, $reverse)
+    {
+        $index = $reverse ? count($_values) - 1 : 0;
+        foreach ($_values as $_value) {
+            if ($_value === $value) { return $index; }
+            $reverse ? $index-- : $index++;
         }
         return null;
     }
-
-    protected final function _indexOf($value): ?int
+    protected final function _indexOf($value)
     {
-        $index = 0;
-        foreach ($this->getArrayCopy() as $key => $value) {
-            //
-        }
+        return $this->_index($value, $this->values(), false);
     }
-    protected final function _lastIndexOf($value): ?int
-    {}
+    protected final function _lastIndexOf($value)
+    {
+        return $this->_index($value, array_reverse($this->values()), true);
+    }
 
-    protected final function _has($value): bool
+    protected final function _has($value)
     {
         return in_array($value, $this->values(), true);
     }
-    protected final function _hasKey($key): bool
+    protected final function _hasKey($key)
     {
-        return in_array($value, $this->values(), true);
+        return in_array($key, $this->keys(), true);
     }
     // @alias
-    protected final function _hasValue($value): bool
+    protected final function _hasValue($value)
     {
         return $this->_has($value);
     }
 
-    protected final function _set($key, $value): self
+    protected final function _set($key, $value)
     {
         $this->readOnlyCheck();
         $this->offsetSet($key, $value);
@@ -52,17 +56,17 @@ trait ArrayTrait
     }
     protected final function _get($key, $valueDefault = null, &$ok = null)
     {
-        if ($ok = $this->offsetExists($key)) {
+        if ($ok = $this->_hasKey($key)) {
             $value = $this->offsetGet($key);
         }
         return $value ?? $valueDefault;
     }
 
-    protected function _add($value, &$size = null): self
+    protected function _add($value, &$size = null)
     {
         return $this->_append($value, $size);
     }
-    protected function _remove($value, &$ok = null): self
+    protected function _remove($value, &$ok = null)
     {
         $this->readOnlyCheck();
         if ($ok = (($key = $this->_search($value)) !== null)) {
@@ -70,24 +74,31 @@ trait ArrayTrait
         }
         return $this;
     }
-    protected final function _removeAt($key, &$ok = null): self
+    protected final function _removeAt($key, &$ok = null)
     {
         $this->readOnlyCheck();
-        if ($ok = $this->offsetExists($key)) {
+        if ($ok = $this->_hasKey($key)) {
             $this->offsetUnset($key);
         }
         return $this;
     }
+    protected final function _removeAll($values, &$count = null)
+    {
+        foreach ($values as $value) {
+            while ($this->remove($value, $ok) && $ok) { $count++; }
+        }
+        return $this;
+    }
 
-    protected final function _append($value, &$size = null): self
+    protected final function _append($value, &$size = null)
     {
         return $this->_unpop($value, $size);
     }
-    protected final function _prepend($value, &$size = null): self
+    protected final function _prepend($value, &$size = null)
     {
         return $this->_unshift($value, $size);
     }
-    protected final function _delete($value, &$size = null): self
+    protected final function _delete($value, &$size = null)
     {
         $this->_remove($value, $ok);
         $size = $this->size();
@@ -101,7 +112,7 @@ trait ArrayTrait
         $size = $this->size();
         return $value;
     }
-    protected final function _unpop($value, &$size = null): self
+    protected final function _unpop($value, &$size = null)
     {
         $this->readOnlyCheck();
         // ...
@@ -116,7 +127,7 @@ trait ArrayTrait
         $size = $this->size();
         return $value;
     }
-    protected final function _unshift($key, $value, &$size = null): self
+    protected final function _unshift($key, $value, &$size = null)
     {
         $this->readOnlyCheck();
         // ...
@@ -124,14 +135,14 @@ trait ArrayTrait
         return $this;
     }
 
-    protected final function _put($key, $value): self
+    protected final function _put($key, $value)
     {
         $this->readOnlyCheck();
         // ...
         $this->items[$key] = $value;
         return $this;
     }
-    protected final function _push($key, $value): self
+    protected final function _push($key, $value)
     {
         $this->readOnlyCheck();
         // ...
