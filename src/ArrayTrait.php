b321892c385037714006906f8251ea7787a1abd0
Kerem Güneş <k-gun@mail.com>
2019-02-12 00:36:47 +0300

    Add docs, add argument & return types.

--- src/ArrayTrait.php
+++ src/ArrayTrait.php
@@ -26,6 +26,7 @@ declare(strict_types=0);
 
 namespace xobjects;
 
+use xobjects\ArrayInterface;
 use Closure;
 
 /**
@@ -35,83 +36,180 @@ use Closure;
  */
 trait ArrayTrait
 {
-    // memory-wise index, key search
-    private final function _index($searchValue, $reverse)
+    /**
+     * Index (memory-wise index, key search).
+     * @param  any  $searchValue
+     * @param  bool $reverse
+     * @return ?array
+     */
+    private final function _index($searchValue, bool $reverse): ?array
     {
-        if (!$reverse) { $index = 0;
+        if (!$reverse) {
+            $index = 0;
             foreach ($this->generate() as $key => $value) {
-                if ($value === $searchValue) { return [$index, $key]; } $index++;
+                if ($value === $searchValue) {
+                    return [$index, $key];
+                }
+                $index++;
             }
-        } else { $index = $this->size() - 1;
+        } else {
+            $index = $this->size() - 1;
             foreach ($this->generate(true) as $key => $value) {
-                if ($value === $searchValue) { return [$index, $key]; } $index--;
+                if ($value === $searchValue) {
+                    return [$index, $key];
+                }
+                $index--;
             }
         }
         return null;
     }
-    // @return int|string|null
+
+    /**
+     * Search.
+     * @param  any $value
+     * @return ?int|?string
+     */
     protected final function _search($value)
     {
         return $this->_index($value, false)[1] ?? null;
     }
+
+    /**
+     * Search last.
+     * @param  any $value
+     * @return ?int|?string
+     */
     protected final function _searchLast($value)
     {
         return $this->_index($value, true)[1] ?? null;
     }
-    protected final function _indexOf($value)
+
+    /**
+     * Index of.
+     * @param  any $value
+     * @return ?int
+     */
+    protected final function _indexOf($value): ?int
     {
         return $this->_index($value, false)[0] ?? null;
     }
-    protected final function _lastIndexOf($value)
+
+    /**
+     * Last index of.
+     * @param  any $value
+     * @return ?int
+     */
+    protected final function _lastIndexOf($value): ?int
     {
         return $this->_index($value, true)[0] ?? null;
     }
 
-    protected final function _has($value)
+    /**
+     * Has.
+     * @param  any $value
+     * @return bool
+     */
+    protected final function _has($value): bool
     {
         return $this->_indexOf($value) !== null;
     }
-    protected final function _hasKey($key, $keyCheck = true)
+
+    /**
+     * Has key.
+     * @param  int|string $key
+     * @param  bool       $keyCheck @internal
+     * @return bool
+     */
+    protected final function _hasKey($key, bool $keyCheck = true): bool
     {
         $keyCheck && $this->keyCheck($key);
+
         return in_array($key, $this->keys(), true);
     }
 
-    protected final function _set($key, $value, &$size = null)
+    /**
+     * Set.
+     * @param  int|string $key
+     * @param  any        $value
+     * @param  int|null  &$size
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _set($key, $value, int &$size = null): ArrayInterface
     {
         $this->keyCheck($key);
         $this->stackCommand('set', $key, $value, $size);
+
         return $this;
     }
-    protected final function _get($key, $valueDefault = null, &$found = null)
+
+    /**
+     * Get.
+     * @param  int|strings $key
+     * @param  any|null    $valueDefault
+     * @param  bool|null  &$found
+     * @return any|null
+     */
+    protected final function _get($key, $valueDefault = null, bool &$found = null)
     {
         $this->keyCheck($key);
+
         if ($found = $this->_hasKey($key, false)) {
             $this->stackCommand('get', $key, $value);
         }
+
         return $value ?? $valueDefault;
     }
 
-    protected final function _add($value)
+    /**
+     * Add.
+     * @param  any $value
+     * @return xobjects\ArrayInterface
+     *
+     */
+    protected final function _add($value): ArrayInterface
     {
         return $this->_unpop($value);
     }
-    protected final function _remove($value, &$found = null)
+
+    /**
+     * Remove.
+     * @param  any        $value
+     * @param  bool|null &$found
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _remove($value, bool &$found = null): ArrayInterface
     {
         if ($found = (null !== $key = $this->_search($value))) {
             $this->stackCommand('unset', $key);
         }
+
         return $this;
     }
-    protected final function _removeAt($key, &$found = null)
+
+    /**
+     * Remove at.
+     * @param  int|string  $key
+     * @param  bool|null  &$found
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _removeAt($key, bool &$found = null): ArrayInterface
     {
         $this->keyCheck($key);
+
         if ($found = $this->_hasKey($key, false)) {
             $this->stackCommand('unset', $key);
         }
+
         return $this;
     }
-    protected final function _removeAll($values, &$count = null)
+
+    /**
+     * Remove all.
+     * @param  array     $values
+     * @param  int|null &$count
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _removeAll(array $values, int &$count = null): ArrayInterface
     {
         foreach ($values as $value) {
             while (null !== $key = $this->_search($value)) {
@@ -119,112 +217,239 @@ trait ArrayTrait
                 $count++;
             }
         }
+
         return $this;
     }
 
-    protected final function _append($value, &$size = null)
+    /**
+     * Append.
+     * @param  any       $value
+     * @param  int|null &$size
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _append($value, int &$size = null): ArrayInterface
     {
         return $this->_unpop($value, $size);
     }
-    protected final function _prepend($value, &$size = null)
+
+    /**
+     * Prepend.
+     * @param  any       $value
+     * @param  int|null &$size
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _prepend($value, &$size = null): ArrayInterface
     {
         return $this->_unshift($value, $size);
     }
 
-    protected final function _pop(&$size = null)
+    /**
+     * Pop.
+     * @param  int|null &$size
+     * @return any
+     */
+    protected final function _pop(int &$size = null)
     {
         $this->stackCommand('pop', $value, $size);
+
         return $value;
     }
-    protected final function _unpop($value, &$size = null)
+
+    /**
+     * Unpop.
+     * @param  any       $value
+     * @param  int|null &$size
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _unpop($value, int &$size = null): ArrayInterface
     {
         $this->stackCommand('unpop', $value, $size);
-        return $this;
-    }
-    protected final function _shift(&$size = null)
-    {
-        $this->stackCommand('shift', $value, $size);
-        return $value;
-    }
-    protected final function _unshift($value, &$size = null)
-    {
-        $this->stackCommand('unshift', $value, $size);
+
         return $this;
     }
 
-    protected final function _put($key, $value)
+    /**
+     * Shift.
+     * @param  int|null &$size
+     * @return any
+     */
+    protected final function _shift(int &$size = null)
+    {
+        $this->stackCommand('shift', $value, $size);
+
+        return $value;
+    }
+
+    /**
+     * Unshift.
+     * @param  any       $value
+     * @param  int|null &$size
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _unshift($value, int &$size = null): ArrayInterface
+    {
+        $this->stackCommand('unshift', $value, $size);
+
+        return $this;
+    }
+
+    /**
+     * Put.
+     * @param  int|string $key
+     * @param  any        $value
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _put($key, $value): ArrayInterface
     {
         $this->keyCheck($key);
         $this->stackCommand('put', $key, $value);
+
         return $this;
     }
-    protected final function _push($key, $value)
+
+    /**
+     * Push.
+     * @param  int|string $key
+     * @param  any        $value
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _push($key, $value): ArrayInterface
     {
         $this->keyCheck($key);
         $this->stackCommand('push', $key, $value);
+
         return $this;
     }
-    protected final function _pull($key, $valueDefault = null, &$found = null)
+
+    /**
+     * Pull.
+     * @param  int|string  $key
+     * @param  any|null    $valueDefault
+     * @param  bool|null  &$found
+     * @return any|null
+     */
+    protected final function _pull($key, $valueDefault = null, bool &$found = null)
     {
         $this->keyCheck($key);
+
         if ($found = $this->_hasKey($key, false)) {
             $this->stackCommand('pull', $key, $value);
         }
+
         return $value ?? $valueDefault;
     }
 
-    protected final function _replace($value, $replaceValue, &$found = null)
+    /**
+     * Replace.
+     * @param  any        $value
+     * @param  any        $replaceValue
+     * @param  bool|null &$found
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _replace($value, $replaceValue, bool &$found = null): ArrayInterface
     {
         if ($found = (null !== $key = $this->_search($value))) {
             $this->_put($key, $replaceValue);
         }
+
         return $this;
     }
-    protected final function _replaceAt($key, $replaceValue, &$found = null)
+
+    /**
+     * Replace at.
+     * @param  int|string  $key
+     * @param  any         $replaceValue
+     * @param  bool|null  &$found
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _replaceAt($key, $replaceValue, bool &$found = null): ArrayInterface
     {
         $this->keyCheck($key);
+
         if ($found = $this->_hasKey($key, false)) {
             $this->_put($key, $replaceValue);
         }
+
         return $this;
     }
 
+    /**
+     * Find.
+     * @param  Closure $func
+     * @return any|null
+     */
     protected final function _find(Closure $func)
     {
         foreach ($this->generate() as $key => $value) {
-            if ($func($key, $value)) { return $value; }
-        }
-        return null;
-    }
-    protected final function _findKey(Closure $func)
-    {
-        foreach ($this->generate() as $key => $value) {
-            if ($func($key, $value)) { return $key; }
-        }
-        return null;
-    }
-    protected final function _findIndex(Closure $func)
-    {
-        $index = 0;
-        foreach ($this->generate() as $key => $value) {
-            if ($func($key, $value)) { return $index; } $index++;
+            if ($func($value, $key)) {
+                return $value;
+            }
         }
         return null;
     }
 
-    protected final function _pad(int $times, $value)
+    /**
+     * Find key.
+     * @param  Closure $func
+     * @return int|string
+     */
+    protected final function _findKey(Closure $func)
+    {
+        foreach ($this->generate() as $key => $value) {
+            if ($func($value, $key)) {
+                return $key;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Find index.
+     * @param  Closure $func
+     * @return ?int
+     */
+    protected final function _findIndex(Closure $func): ?int
+    {
+        $index = 0;
+        foreach ($this->generate() as $key => $value) {
+            if ($func($value, $key)) {
+                return $index;
+            }
+            $index++;
+        }
+
+        return null;
+    }
+
+    /**
+     * Pad.
+     * @param  int $times
+     * @param  any $value
+     * @return xobjects\ArrayInterface
+     */
+    protected final function _pad(int $times, $value): ArrayInterface
     {
         while ($times--) {
             $this->_unpop($value);
         }
+
         return $this;
     }
-    protected function _fill(int $times, $value, int $offset = null)
+
+    /**
+     * Fill.
+     * @param  int      $times
+     * @param  any      $value
+     * @param  int|null $offset
+     * @return xobjects\ArrayInterface
+     */
+    protected function _fill(int $times, $value, int $offset = null): ArrayInterface
     {
         $offset = $offset ?? $this->size();
         for ($i = 0; $i < $times; $i++) {
             $this->_set($offset + $i, $value);
         }
+
         return $this;
     }
 }
