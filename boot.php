013e04bfef1ecea167bb76ff76f532fc63de8479
Kerem Güneş <k-gun@mail.com>
2019-02-12 16:12:38 +0300

    Add boot file.

--- boot.php
+++ boot.php
@@ -4,6 +4,6 @@
  * @private
  */
 (function() {
-    $autoload = require_once __dir__ .'/Autoload.php';
+    $autoload = require_once __dir__ .'/src/Autoload.php';
     $autoload->register();
 })();
